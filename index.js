855828898a6d8a0a70788408b724bd3bd1d3498e
Jaeri Park <motonimjrp@gmail.com>
2021-05-03 17:11:12 -0400

    adding index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('.slider-inner').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '0px',
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed: 1500,
+  });
+});
